6251143a6877a9481f1833c7e0fb68af6492004d
Chua Yu Jia <blacknoirxxi@gmail.com>
2019-07-02 17:29:18 +0800

    BT19: setup a UE log to display if player controller is possessing a tank

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,19 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s"), *ControlledTank ->GetName());
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
